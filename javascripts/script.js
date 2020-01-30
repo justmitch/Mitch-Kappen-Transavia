063b548437ebe8217947169018a264c5916d3517
Mitch <mitchkappen@gmail.com>
2020-01-30 21:01:34 +0100

    NieuwsteVersie30Jan
    Dit is de laatste versie(30 jan)

---
+++ javascripts/script.js
@@ -0,0 +1,26 @@
+var body = document.querySelector('body');
+var btnFilterOpen = document.querySelector('header button');
+var btnFilterClose = document.querySelector('form button');
+
+btnFilterOpen.addEventListener('click', openFilter);
+btnFilterClose.addEventListener('click', closeFilter);
+
+function openFilter() {
+  body.classList.add('filter');
+}
+
+function closeFilter(e) {
+  e.preventDefault()
+  body.classList.remove('filter');
+}
+
+<!--Functie voor het afspelen en pauzeren audio op detailpagina-->
+                    var vid = document.getElementById("myVideo");
+
+                    function playVid() {
+                      vid.play();
+                    }
+
+                    function pauseVid() {
+                      vid.pause();
+                    }
